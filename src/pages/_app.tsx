310f25cef50ea56b9b0df0fe6662690d15b7ac15
{cocoide} <{cocoide.com@gmail.com}>
2022-11-22 20:06:41 +0900

    create article page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import Header from '../components/Header'
-import AuthWrapper from '../utils/AuthWrapper';
+import AuthWrapper from '../utils/Auth/AuthWrapper';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/utils/Auth/AuthWrapper.tsx
+++ src/utils/Auth/AuthWrapper.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-import Loading from '../components/Loading';
+import Loading from '../../components/Loading';
 import RouteGuard from './RouteGuard';
 
 // ログイン済みユーザにのみ表示するページ
